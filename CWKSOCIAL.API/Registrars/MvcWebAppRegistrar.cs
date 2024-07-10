fbe72e5eb38ab78911bda3bbb00d3a788dda60a9
giddy11 <edoghotugiddy@gmail.com>
2024-07-10 11:40:03 +0100

    restructured the pipeline structure

--- CWKSOCIAL.API/Registrars/MvcWebAppRegistrar.cs
+++ CWKSOCIAL.API/Registrars/MvcWebAppRegistrar.cs
@@ -1,10 +1,21 @@
 ﻿
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
 namespace CWKSOCIAL.API.Registrars
 {
     public class MvcWebAppRegistrar : IWebApplicationRegistrar
     {
         public void RegisterPipelineComponents(WebApplication app)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                var provider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+                foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", description.ApiVersion.ToString());
+                }
+            });
 
             app.UseHttpsRedirection();
             app.UseAuthorization();
