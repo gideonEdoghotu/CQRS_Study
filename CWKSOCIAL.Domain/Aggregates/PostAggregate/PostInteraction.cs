42c9c6b4b8a4b9416000425571842a5ff808d39e
giddy11 <edoghotugiddy@gmail.com>
2024-07-10 14:04:00 +0100

    implemented rich domain models

---
+++ CWKSOCIAL.Domain/Aggregates/PostAggregate/PostInteraction.cs
@@ -0,0 +1,33 @@
+﻿using CWKSOCIAL.Domain.Aggregates.UserProfileAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CWKSOCIAL.Domain.Aggregates.PostAggregate
+{
+    public class PostInteraction
+    {
+        private PostInteraction()
+        {
+        }
+
+        public static PostInteraction CreatePostInteraction(Guid id, InteractionType type)
+        {
+            // TODO: Add validation, error handling strategies, error notification strategies
+
+            return new PostInteraction()
+            {
+                Id = id,
+                InteractionType = type
+            };
+        }
+
+
+
+        public Guid Id { get; private set; }
+        public Guid PostId { get; private set; }
+        public InteractionType InteractionType { get; private set; }
+    }
+}
